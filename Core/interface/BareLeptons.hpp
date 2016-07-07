bd6d6db55c15247944de7646d52971a4dddcdd1c
Dylan George Hsu <dghsu@mit.edu>
2016-07-07 10:21:29 -0400

    add bit 15 'LepGlobalOrTracker' and add it to the bambu config

--- Core/interface/BareLeptons.hpp
+++ Core/interface/BareLeptons.hpp
@@ -23,7 +23,8 @@ class BareLeptons : virtual public BareP4
           LepVetoIso = 1UL<<11,
           LepLooseIso = 1UL<<12,
           LepMediumIso = 1UL<<13,
-          LepTightIso = 1UL<<14
+          LepTightIso = 1UL<<14,
+          LepGlobalOrTracker = 1UL<<15
         };
 
         BareLeptons();
